4497edfa3a851450bff5280dbf87197d852cf0db
Rachit Nigam <rachit.nigam12@gmail.com>
2016-08-18 18:32:54 -0400

    Added datalog setup to travis.

--- fetch-travis-deps.sh
+++ fetch-travis-deps.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-if [ -x "/home/travis/mydeps/z3/bin/z3" ]; then
-  echo "Not fetching any dependencies"
-  exit 0
-fi
-
 cd /home/travis
 rm -rf mydeps
 mkdir mydeps
@@ -14,3 +8,11 @@ URL=https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/$ZIPFILE
 wget $URL
 unzip $ZIPFILE
 mv z3-4.4.1-x64-ubuntu-14.04 z3
+
+DATALOG=datalog.tar.gz
+wget https://sourceforge.net/projects/datalog/files/latest/download
+mv download $DATALOG
+gunzip $DATALOG
+tar -xvf datalog.tar
+cd datalog-2.6
+sudo ./configure && sudo make && sudo make install
