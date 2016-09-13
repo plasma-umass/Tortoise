b90f21a0f2a5430fce3fed4ccc94c4c3ff063d16
Rachit Nigam <rachit.nigam12@gmail.com>
2016-09-13 17:35:00 -0400

    Update fetch-travis-deps.sh

--- fetch-travis-deps.sh
+++ fetch-travis-deps.sh
@@ -1,22 +1,29 @@
-cd /home/travis
-rm -rf mydeps
-mkdir mydeps
-cd mydeps
+#!/bin/bash
+set -e
+set -x
 
-ZIPFILE=z3-4.4.1-x64-ubuntu-14.04.zip
-URL=https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/$ZIPFILE
-wget $URL
-unzip $ZIPFILE
-mv z3-4.4.1-x64-ubuntu-14.04 z3
+if [ ! -d $HOME/mydeps ]; then
+  mkdir $HOME/mydeps
+fi
 
-#DATALOG=datalog.tar.gz
-#wget https://sourceforge.net/projects/datalog/files/datalog/2.5/datalog-2.5.tar.gz/download
-#mv download $DATALOG
-#gunzip $DATALOG
-#tar -xvf datalog.tar
-#cd datalog-2.5
-#sudo ./configure && sudo make && sudo make install
+cd $HOME/mydeps
 
-wget https://thenigma.github.io/res/datalog
-chmod a+x datalog
-sudo ln -s ./datalog /usr/local/bin/datalog
+if [ ! -x "/home/travis/mydeps/z3/bin/z3" ]; then
+  echo "Fetching Z3 ..."
+  ZIPFILE=z3-4.4.1-x64-ubuntu-14.04.zip
+  URL=https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/$ZIPFILE
+  wget $URL
+  unzip $ZIPFILE
+  mv z3-4.4.1-x64-ubuntu-14.04 z3
+fi
+
+if [ ! -x "/home/travis/mydeps/datalog-2.5/datalog" ]; then
+    echo "Fetching Datalog ..."
+    wget -O datalog.tar.gz https://sourceforge.net/projects/datalog/files/datalog/2.5/datalog-2.5.tar.gz/download
+    tar xzf datalog.tar.gz
+    pushd datalog-2.5
+    ./configure
+    sed -i "s/-DHAVE_LIBREADLINE=1//g" Makefile
+    make
+    popd
+fi
