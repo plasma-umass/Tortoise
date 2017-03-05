576233eb4e9d72401124aa5e942c8018d360581a
Aaron Weiss <aaronweiss74@gmail.com>
2017-03-05 14:44:27 -0600

    Updated clean-manifest to output using single quotes, instead of double quotes.

--- clean-manifest.sh
+++ clean-manifest.sh
@@ -3,5 +3,6 @@
 cp $1 $1.bak
 sed "s/'/\"/g" $1 > $1.new
 perl -pe 's/(\$[a-zA-Z0-9_\-]*)(?=(?:(?:[^\"\(]*+[\"\(]+){2})*+[^\"\)]*+\z)/\"$1\"/mg' \
-    <<< $(cat $1.new) | tee $1 > /dev/null
-rm $1.new
+    <<< $(cat $1.new) | tee $1.updated > /dev/null
+sed "s/\"/'/g" $1.updated > $1
+rm $1.new $1.updated
