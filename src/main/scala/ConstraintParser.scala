12e4618ef9f054ea631584e1603579875635e067
Aaron Weiss <aaronweiss74@gmail.com>
2017-04-16 20:50:58 -0400

    Added a parser for constraints.

---
+++ src/main/scala/ConstraintParser.scala
@@ -0,0 +1,52 @@
+package pup
+
+import scala.util.parsing.combinator._
+
+import SymbolicFS._
+
+private class ConstraintParser extends RegexParsers with PackratParsers {
+  type P[T] = PackratParser[T]
+
+  lazy val number: P[String] = "" ~> "[0-9]+".r
+  lazy val doubleQuotedString: P[String] = "\"" ~> "[^\"]*".r <~ "\""
+  lazy val singleQuotedString: P[String] = "\'" ~> "[^\']*".r <~ "\'"
+
+  lazy val path: P[String] =
+    doubleQuotedString | singleQuotedString
+
+  lazy val state: P[FileState] =
+    "file" ^^ { _ => File } |
+    "dir" ^^  { _ => Dir  } |
+    "nil" ^^  { _ => Nil  }
+
+  lazy val contents: P[String] =
+    doubleQuotedString | singleQuotedString
+
+  lazy val mode: P[String] =
+    number
+
+  lazy val stateConstraint: P[StateConstraint] =
+    path ~ ("->" ~> state) ^^ { case path ~ state => StateConstraint(path, state) }
+
+  lazy val contentsConstraint: P[ContentsConstraint] =
+    path ~ ("=>" ~> contents) ^^ { case path ~ contents => ContentsConstraint(path, contents) }
+
+  lazy val modeConstraint: P[ModeConstraint] =
+    path ~ ("~>" ~> mode) ^^ { case path ~ mode => ModeConstraint(path, mode) }
+
+  lazy val constraint: P[Constraint] =
+    stateConstraint | contentsConstraint | modeConstraint
+
+  lazy val constraints: P[Seq[Constraint]] =
+    repsep(constraint, ",")
+}
+
+object ConstraintParser {
+  private val parser = new ConstraintParser()
+  import parser._
+
+  def parse(str: String): Seq[Constraint] = parseAll(constraints, str) match {
+    case Success(res, _) => res
+    case res => throw new ParseError(s"$res")
+  }
+}
